e962973b403cef70f2333025d82766407b0c4a2f
ZannelleOoko <ookozannelle@gmail.com>
2025-07-11 13:51:42 +0300

    undo js chunking

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { visualizer } from 'rollup-plugin-visualizer'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), visualizer({ open: true })],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) return 'vendor'
-          if (id.includes('react')) return 'react'
-        }
-      }
-    }
-  }
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
